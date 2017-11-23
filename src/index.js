bddf404b38136a4ed57ab6002a7a1fb8630f8fdb
Jan Mertlik <jm8801@mun.ca>
2017-11-23 19:48:07 -0330

    index.js changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDom from 'react-dom'
 import './App.css';
 
 class App extends Component {
@@ -6,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title"> Hello World! </h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/app.js</code> and save to reload.
