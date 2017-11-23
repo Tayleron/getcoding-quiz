4e7460073003804fcc053bc65f980ffb5f5df7a7
Jan Mertlik <jm8801@mun.ca>
2017-11-23 19:41:50 -0330

    fixed it

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '../public/index.css';
+import React, { Component } from 'react';
+import './App.css';
 
-function test(){
-
-    return "hello world";
-
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/app.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
 }
+
+export default App;
