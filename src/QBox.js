d9e51dbbd264f8bc2937fd0ccc60d59726e6a605
Tayleron <tayleron@gmail.com>
2017-11-28 13:18:57 -0330

    Progress of QBox. Attempting to add listing feature to facilitate adding of multiple ABox's via button.

--- src/QBox.js
+++ src/QBox.js
@@ -1,14 +1,42 @@
 import React from "react";
 import ABox from './ABox.js';
 
-//fuction addAnswer
+function answersList(props) {
+    const answers = props.answers;
+    const listItems = answers.map((answer) =>
+        <li key={answers.toString()}>
+            {answers}
+        </li>
+    );
+    return (
+        <ul>{listItems}</ul>
+    );
+}
 class QBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            answers: []
+        };
+    }
+    
+    handleAddAnswer(props) {
+        this.setState({
+            answers: <ABox />
+        }, function() {
+            console.log('Answer Added');
+        });
+    }
+
     render() {
+        const answers = []
+
         return (
             <div className="question">
                 Question<input type="text" className="qText"/>
-                <ABox />          
-                <button className="addAnswer">+ Answer</button>
+                <answersList listItems={answers} />
+                <ABox />
+                <button onClick={() => this.handleAddAnswer()}>+ Answer</button>
             </div>
         )
     };
