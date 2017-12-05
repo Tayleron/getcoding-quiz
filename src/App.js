5b576fd603eef26744f891e531c14f38ef22fdf7
jthetzel <jthetzel@gmail.com>
2017-12-05 19:12:44 -0330

    Update state structure, indentation

--- src/App.js
+++ src/App.js
@@ -4,69 +4,67 @@ import QuestionsBlock from './QuestionsBlock.js';
 import ResultsBlock from './ResultsBlock.js';
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            myQuiz: {
-                "title" : {
-                    "title" : "",
-                    "desc" : ""
-                },
-                "results" : [
-                    {
-                    "id" : "",
-                    "title" : "",
-                    "desc" : "",
-                    },
-                ],
-                "questions" : [
-                    {
-                    "id" : "",
-                    "question" : "",
-                        "answers" : [
-                            {
-                            "id" : "",
-                            "text" : "",
-                            "corResult" : "",
-                            },
-                        ]              
-                    },
-                ]
+  constructor(props) {
+    super(props)
+    this.state = {
+      "title" : "",
+      "desc" : "",
+      "results" : [
+        {
+          "id" : "",
+          "title" : "",
+          "desc" : ""
+        }
+      ],
+      "questions" : [
+        {
+          "id" : "",
+          "question" : "",
+          "answers" : [
+            {
+              "id" : "",
+              "text" : "",
+              "corResult" : ""
             }
-        };
+          ]
+        }
+      ]
     }
+  }
 
-    handleChange(event) {        
-        const myQuiz = this.state.myQuiz
-        const value = event.target.value
-        const name = event.target.name
+  handleChange(event) {        
+    // const myQuiz = this.state.myQuiz
+    const value = event.target.value
+    const name = event.target.name
+    console.log(name, value)
 
-        myQuiz.title[name] = value
-        
-        this.setState(
-            {myQuiz: myQuiz},
-            function(){console.log(this.state)}
-        );
-    };
+    // myQuiz.title[name] = value
+    
+    // this.setState(
+    //   {myQuiz: myQuiz},
+    //   function(){console.log(this.state)}
+    // );    
+  };
 
-    render() {
-        const title = this.state.title;
-        const desc = this.state.desc;
-        return (
-            <div><h3>App Block</h3>
-                <TitleBlock 
-                    title={title} 
-                    desc={desc}
-                    onChange={(event) => this.handleChange(event)} 
-                />
-                <ResultsBlock />
-                <QuestionsBlock />
-                <button>
-                    Save
-                </button>
-            </div>
-        )
-    }
+  render() {
+    console.log(this.state)
+    const title = this.state.title;
+    const desc = this.state.desc;
+    return (
+      <div><h3>App Block</h3>
+        <TitleBlock 
+          title={title} 
+          desc={desc}
+          onChange={(event) => this.handleChange(event)} 
+          />
+          <ResultsBlock />
+          <QuestionsBlock />
+          <button>
+            Save
+          </button>
+      </div>
+    )
+  }
 }
 
 export default App
--- src/TitleBlock.js
+++ src/TitleBlock.js
@@ -3,38 +3,38 @@ import React, { Component } from 'react';
 
 class TitleBlock extends Component {
 
-    handleUpdate(event) {
-        this.props.onChange(event)
-    }
-    
-    render() {
-        const title = this.props.title;
-        const desc = this.props.desc;
-        return (
-            <div>
-                <form>
-                    <label>
-                        Quiz Title
-                        <input 
-                            name="title"
-                            type="text"
-                            value={title}
-                            onChange={(event) => this.handleUpdate(event)}
-                        />
-                    </label>
-                    <label>
-                        Quiz Description
-                        <input 
-                            name="desc"
-                            type="text"
-                            value={desc}
-                            onChange={(event) => this.handleUpdate(event)}
-                        />
-                    </label>
-                </form>
-            </div>
-        )
-    };
+  handleUpdate(event) {
+    this.props.onChange(event)
+  }
+  
+  render() {
+    const title = this.props.title;
+    const desc = this.props.desc;
+    return (
+      <div>
+        <form>
+          <label>
+            Quiz Title
+            <input 
+              name="title"
+              type="text"
+              // value={title}
+              onChange={(event) => this.handleUpdate(event)}
+              />
+          </label>
+          <label>
+            Quiz Description
+            <input 
+              name="desc"
+              type="text"
+              // value={desc}
+              onChange={(event) => this.handleUpdate(event)}
+              />
+          </label>
+        </form>
+      </div>
+    )
+  };
 }
 
 export default TitleBlock
